7ba8fb537075efaf4e5d6d036682c3e0ee5d777f
Łukasz <lukasz921671@gmail.com>
2022-04-12 01:17:55 +0200

    added slider offer on js

--- js/main.js
+++ js/main.js
@@ -12,4 +12,49 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+
+
+    //Offer
+
+    // changing services
+    const serviceList = document.querySelectorAll(".offer__service");
+    const serviceDescriptionList = document.querySelectorAll(".offer__description");
+    const descriptionshow = document.querySelector(".offer__description--show")
+
+    serviceList.forEach(service => {
+        service.addEventListener("click", () => {
+            let activeService = document.querySelector(".offer__service--active")
+            let activeDescription = document.querySelector(".offer__description--active");
+
+            if (service.dataset.service != activeService) {
+                serviceList.forEach(item => {
+                    item.classList.remove("offer__service--active");
+                });
+                service.classList.toggle("offer__service--active");
+
+
+
+                // descriptionshow.classList.remove("offer__description--active");
+                // descriptionshow.innerHTML = serviceDescriptionList[service.dataset.service - 1].innerHTML;
+                // descriptionshow.classList.add("offer__description--active");
+
+
+
+
+                serviceDescriptionList.forEach(item => {
+                    item.classList.remove("offer__description--active");
+                });
+
+                console.log(serviceDescriptionList[service.dataset.service - 1]);
+                serviceDescriptionList[service.dataset.service - 1].classList.add("offer__description--active");
+            }
+
+            // let numberOfService = service.dataset.service;
+            // let numberOfDescription = ;
+
+
+
+        });
+    });
+
 });
